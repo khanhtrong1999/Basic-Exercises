ca7c681f530c40ff9e9985bccaa865b1ad9ca063
khanhtrong1999 <37130643+khanhtrong1999@users.noreply.github.com>
2018-03-10 19:18:14 +0700

    Update bai17.java

--- bai17.java
+++ bai17.java
@@ -3,32 +3,24 @@ import java.util.Scanner;
 public class bai17 {
 	public static void main(String[] args)
 	 {
-	  long binary1, binary2;
-	  int i = 0, remainder = 0;
-	  int[] sum = new int[20];
+	  long binary1, binary2, multiply = 0;
+	  int digit, factor = 1;
 	  Scanner in = new Scanner(System.in);
-
-	  System.out.print("Input first binary number: ");
+	  System.out.print("Input the first binary number: ");
 	  binary1 = in.nextLong();
-	  System.out.print("Input second binary number: ");
+	  System.out.print("Input the second binary number: ");
 	  binary2 = in.nextLong();
-
-	  while (binary1 != 0 || binary2 != 0) 
-	  {
-	   sum[i++] = (int)((binary1 % 10 + binary2 % 10 + remainder) % 2);
-	   remainder = (int)((binary1 % 10 + binary2 % 10 + remainder) / 2);
-	   binary1 = binary1 / 10;
-	   binary2 = binary2 / 10;
-	  }
-	  if (remainder != 0) {
-	   sum[i++] = remainder;
-	  }
-	  --i;
-	  System.out.print("Sum of two binary numbers: ");
-	  while (i >= 0) {
-	   System.out.print(sum[i--]);
-	  }
-	   System.out.print("\n");  
+	  //System.out.println(binary1);
+	  //System.out.println(binary2);
+	  String s = String.valueOf(binary1);
+	  String s2 = String.valueOf(binary2);
+	  System.out.println(s);
+	  System.out.println(s2);
+	  int decimalValue = Integer.parseInt(s, 2);
+	  int decimalValue2 = Integer.parseInt(s2, 2);
+	  int sum = decimalValue + decimalValue2;
+	  //System.out.println(sum);
+	  String stkq = Integer.toBinaryString(sum);
+	  System.out.println("Kq = "+stkq);
 	 }
-
 }
